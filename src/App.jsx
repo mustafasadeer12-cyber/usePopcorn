57a995a0d7e6e2953db77239a33064ab714b77c2
mustafasadeer12-cyber <mustafasadeer12@gmail.com>
2026-09-21 14:00:19 -0700

    refactor #2: extract Search input component

--- src/App.jsx
+++ src/App.jsx
@@ -59,29 +59,37 @@ export default function App() {
     <NavBar />
     <Main />
 
-    
+
     </>
   );
 }
 
 function NavBar() {
-  const [query, setQuery] = useState("");
+  
   return <nav className="nav-bar">
         <div className="logo">
           <span role="img">🍿</span>
           <h1>usePopcorn</h1>
         </div>
-        <input
+
+        <Search />
+
+        <p className="num-results">
+          Found <strong>X</strong> results
+        </p>
+      </nav>
+}
+
+function Search() {
+  
+  const [query, setQuery] = useState("");
+  return <input
           className="search"                                        // component #1 navbar
           type="text"
           placeholder="Search movies..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
-        <p className="num-results">
-          Found <strong>X</strong> results
-        </p>
-      </nav>
 }
 
 
